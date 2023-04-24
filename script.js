254e1b1f771c9e8a6297c850f6adf6e26f46d6c4
Meghna Rao <mrao123@terpmail.umd.edu>
2023-04-24 19:41:27 -0400

    index

---
+++ script.js
@@ -0,0 +1,35 @@
+function filterList(list, query) {
+    return list.filter((item) => {
+      const lowerCaseName = item.name.toLowerCase();
+      const lowerCaseQuery = query.toLowerCase();
+      return lowerCaseName.includes(lowerCaseQuery);
+    });
+}
+
+async function mainEvent() { // the async keyword means we can make API requests
+    const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+    const filterButton = document.querySelector('.filter_button');
+    let currentList = []; 
+    mainForm.addEventListener('submit', async (submitEvent) => {
+        submitEvent.preventDefault(); 
+        console.log('form submission');
+
+        const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
+        currentList = await results.json();
+        console.table(currentList); 
+  });
+
+  filterButton.addEventListener('click', (event) =>{
+    console.log('clicked FilterButton');
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+    
+    console.log(newList);
+  });
+    
+
+}
