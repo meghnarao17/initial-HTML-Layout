b3a2c42a9fde6c94273a175e79c923fbb5a01be2
Meghna Rao <mrao123@terpmail.umd.edu>
2023-04-24 20:18:50 -0400

    changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function filterList(list, query) {
-    return list.filter((item) => {
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = query.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery);
-    });
-}
+// function filterList(list, query) {
+//     return list.filter((item) => {
+//       const lowerCaseName = item.name.toLowerCase();
+//       const lowerCaseQuery = query.toLowerCase();
+//       return lowerCaseName.includes(lowerCaseQuery);
+//     });
+// }
 
 async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
@@ -19,17 +19,17 @@ async function mainEvent() { // the async keyword means we can make API requests
         console.table(currentList); 
   });
 
-  filterButton.addEventListener('click', (event) =>{
-    console.log('clicked FilterButton');
+//   filterButton.addEventListener('click', (event) =>{
+//     console.log('clicked FilterButton');
 
-    const formData = new FormData(mainForm);
-    const formProps = Object.fromEntries(formData);
+//     const formData = new FormData(mainForm);
+//     const formProps = Object.fromEntries(formData);
 
-    console.log(formProps);
-    const newList = filterList(currentList, formProps.resto);
+//     console.log(formProps);
+//     const newList = filterList(currentList, formProps.resto);
     
-    console.log(newList);
-  });
+//     console.log(newList);
+//   });
     
 
 }
