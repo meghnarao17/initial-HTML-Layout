ed81ae9f0952f669c161e93923244112cb7b5679
Meghna Rao <mrao123@terpmail.umd.edu>
2023-04-27 17:51:41 -0400

    js

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
-// function filterList(list, query) {
-//     return list.filter((item) => {
-//       const lowerCaseName = item.name.toLowerCase();
-//       const lowerCaseQuery = query.toLowerCase();
-//       return lowerCaseName.includes(lowerCaseQuery);
-//     });
-// }
+function initChart(){
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      };
+    
+      const config = {
+        type: 'line',
+        data: data,
+        options: {}
+      };
+
+      return new Chart(
+        chart,
+        config
+      );
+}
+
 
 async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-    const visualization = document.querySelector('.visualization');
-    let currentList = []; 
+    const submit = document.querySelector('#form_button')
+    const chartTarget = document.querySelector('#myChart');
+    submit.style.display = 'none';
+    
+    /* API data request */
+    const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
+    const arrayFromJson = await results.json(); //convert it to JSON
+    
+    initChart(chartTarget);
+
+    if (arrayFromJson.data.length > 0) {
+        submit.style.display = 'block';
+    }
+    
+    let currentArray;
     mainForm.addEventListener('submit', async (submitEvent) => {
         submitEvent.preventDefault(); 
-        console.log('form submission');
+        currentArray = processSpending(arrayFromJson.data);
 
-        const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
-        currentList = await results.json();
-        console.table(currentList); 
   });
 
-  visualization.addEventListener('click', (event) =>{
-     console.log('clicked FilterButton');
+  
 
 //     const formData = new FormData(mainForm);
 //     const formProps = Object.fromEntries(formData);
@@ -29,7 +63,7 @@ async function mainEvent() { // the async keyword means we can make API requests
 //     const newList = filterList(currentList, formProps.resto);
     
 //     console.log(newList);
-  });
+  
     
 
 }
