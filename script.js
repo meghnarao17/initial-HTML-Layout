c7d0dfaabf271c5f724d41ab88ed55c7f946eec0
Meghna Rao <mrao123@terpmail.umd.edu>
2023-04-27 18:25:50 -0400

    initChart

--- script.js
+++ script.js
@@ -32,23 +32,24 @@ function initChart(chart){
 
 
 async function mainEvent() { // the async keyword means we can make API requests
-    const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+    const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     const submit = document.querySelector('#form_button')
     const chartTarget = document.querySelector('#myChart');
     submit.style.display = 'none';
     
+    initChart(chartTarget);
     /* API data request */
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
     const arrayFromJson = await results.json(); //convert it to JSON
     
-    initChart(chartTarget);
+    
 
     if (arrayFromJson.data.length > 0) {
         submit.style.display = 'block';
     }
     
     let currentArray;
-    mainForm.addEventListener('submit', async (submitEvent) => {
+    form.addEventListener('submit', async (submitEvent) => {
         submitEvent.preventDefault(); 
         currentArray = processSpending(arrayFromJson.data);
 
