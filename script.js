2cfc8ac09b49ba1ec78e1f95142e2a6dba3a1740
Meghna Rao <mrao123@terpmail.umd.edu>
2023-05-10 20:10:45 -0400

    JS new - from lab 6

--- script.js
+++ script.js
@@ -30,41 +30,102 @@ function initChart(chart){
       );
 }
 
+function getRandomIntInclusive(min, max){
+  min = Math.ceil(min);
+  max = Math.floor(max); 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function injectHTML(list) {
+  console.log('fired injectHTML')
+  const target = document.querySelector('#restaurant_list')
+  target.innerHTML = '';
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str
+  })
+}
+
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+
+function cutRestaurantList(list) {
+  console.log('fired cut list');
+  const range = [...Array(15).keys()]
+  return newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length -1);
+    return list[index]
+  })
+}
+
 
 async function mainEvent() { // API request
-    const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-    const submit = document.querySelector('#form_button')
+    const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+    const filterDataButton = document.querySelector('#filter');
+    const loadDataButton = document.querySelector('#data_load');
+    const generateListButton = document.querySelector('#generate');
     const chartTarget = document.querySelector('#myChart');
-    submit.style.display = 'none';
-    
-    initChart(chartTarget);
-    /* API data request */
-    const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
-    const arrayFromJson = await results.json(); //convert it to JSON
-    
     
 
-    if (arrayFromJson.data.length > 0) {
+    const loadAnimation = document.querySelector('#data_load_animation');
+    loadAnimation.style.display = 'none';
+
+    let currentList = []; // this is "scoped" to the main event function
+    
+    loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+      console.log('Loading Data'); 
+      loadAnimation.style.display = 'inline-block';
+    
+
+      initChart(chartTarget);
+    /* API data request */
+      const results = await fetch('https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json');
+      
+
+      currentList = await results.json();
+      loadAnimation.style.display = 'none';
+      console.table(currentList); 
+      injectHTML(currentList);
+     });
+
+     
+     filterDataButton.addEventListener('click', (event) =>{
+      console.log('clicked FilterButton');
+  
+      const formData = new FormData(mainForm);
+      const formProps = Object.fromEntries(formData);
+  
+      console.log(formProps);
+      const newList = filterList(currentList, formProps.resto);
+      console.log(newList);
+      injectHTML(newList);
+    });
+
+    generateListButton.addEventListener('click', (event) => {
+      console.log('generate new list');
+      const restaurantsList = cutRestaurantList(currentList);
+      console.log(restaurantsList);
+      injectHTML(restaurantsList);
+      
+  });
+    
+  };
+
+    
+    
+    
+      if (arrayFromJson.data.length > 0) {
         submit.style.display = 'block';
     }
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
     
-    let currentArray;
-    form.addEventListener('submit', async (submitEvent) => {
-        submitEvent.preventDefault(); 
-        currentArray = processSpending(arrayFromJson.data);
-
-  });
-
-  
-
-//     const formData = new FormData(mainForm);
-//     const formProps = Object.fromEntries(formData);
-
-//     console.log(formProps);
-//     const newList = filterList(currentList, formProps.resto);
     
-//     console.log(newList);
   
     
 
-}
